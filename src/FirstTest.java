73024003b943e82d4cc6871bb65646346100682f
Maxim <msgvozdev@gmail.com>
2024-02-22 22:55:29 +0100

    Add the test code

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,8 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import java.io.FileInputStream;
 import java.net.URL;
+import java.util.Properties;
 
 public class FirstTest {
 
@@ -13,17 +16,30 @@ public class FirstTest {
     @Before
     public void setUp() throws Exception {
 
+        // Получить путь к apk-файлу из файла config.properties
+        String configFilePath = "src/config.properties";
+        FileInputStream propertiesInput = new FileInputStream(configFilePath);
+
+        Properties properties = new Properties();
+        properties.load(propertiesInput);
+
+        String apk = properties.getProperty("APK_FILE");
+
+        DesiredCapabilities capabilities = getDesiredCapabilities(apk);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    private static DesiredCapabilities getDesiredCapabilities(String apk) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("appium:platformName", "Android");
-        capabilities.setCapability("appium:deviceName", "AndroidTestDevice");
-        capabilities.setCapability("appium:platformVersion", "14");
-        capabilities.setCapability("appium:automationName", "UiAutomator2");
-        capabilities.setCapability("appium:appPackage", "org.wikipedia");
-        capabilities.setCapability("appium:appActivity", ".main.MainActivity");
-        capabilities.setCapability("appium:app", "/Users/maxim/git/m-a-x-im/OldJavaAppiumAutomation/apk_files/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", apk);
+        return capabilities;
     }
 
     @After
@@ -32,7 +48,17 @@ public class FirstTest {
     }
 
     @Test
-    public void firstTest() {
-        System.out.println("First test");
+    public void firstTest() throws InterruptedException {
+        WebElement skip_onboarding = driver.findElementByXPath("//*[@text='Skip']");
+        skip_onboarding.click();
+
+        WebElement search_input = driver.findElementByXPath("//*[@text='Search Wikipedia']");
+        search_input.click();
+//        Следующая строка вместо ввода текста почему-то нажимает кнопку смены языка:
+//        search_input.sendKeys("Appium");
+//        А вот эта работает, как ожидалось:
+        driver.getKeyboard().sendKeys("Appium");
+
+        Thread.sleep(1000);
     }
 }
